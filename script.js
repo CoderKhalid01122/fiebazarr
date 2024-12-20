53e3576539fb82dafd3a06b04cc097c074347c77
CoderKhalid01122 <saifullah724258@gmail.com>
2024-12-20 21:13:30 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+document.querySelectorAll(".product button").forEach((button) => {
+    button.addEventListener("click", (e) => {
+        const productName = e.target.parentElement.querySelector("h3").innerText;
+        alert(`${productName} has been added to your cart!`);
+    });
+});
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
